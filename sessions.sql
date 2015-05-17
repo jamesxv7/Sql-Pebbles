572316383566101c0bd8a0fb9dae09c65b6f69ed
Jaime Olmo <jamesxv7@gmail.com>
2015-05-16 22:25:57 -0400

    Create sessions.sql

---
+++ sessions.sql
@@ -0,0 +1,7 @@
+-- MySql
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` varchar(32) NOT NULL,
+  `access` int(10) unsigned DEFAULT NULL,
+  `data` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
